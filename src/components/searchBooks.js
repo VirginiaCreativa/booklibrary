662f197fc976c8f76a9dbce9acd1dcddc4116ee2
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2018-03-09 16:20:57 -0500

    KeyPress Search

--- src/components/searchBooks.js
+++ src/components/searchBooks.js
@@ -4,13 +4,27 @@ import { Button, Form, Input } from 'reactstrap';
 
 class SearchBooks extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			query: ''
+		}
+	}
 	search(){
-		console.log('ev');
+		console.log('ev', this.state.query);
 	}
 	render() {
 		return (
 			<Form>
-				<Input type="search" placeholder="Search" />
+				<Input type="search" placeholder="Search" 
+					onChange={event => this.setState({query: event.target.value})}
+					onKeyPress={ev => {
+						let keycode = (ev.keyCode ? ev.keyCode : ev.which);
+						if (keycode === 13) {
+							this.search();
+						}
+					}}
+				/>
 				<Button onClick={(ev) => this.search()} className="btnSearch"><i className="ion-ios-search"></i></Button>
 			</Form>
 		);
